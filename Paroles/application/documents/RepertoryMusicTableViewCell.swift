6741475d8989bea9d84313b46c5af2eade5ac4b3
Benjamin DENEUX <benjamin.deneux@ubleam.com>
2020-02-29 17:04:38 +0100

    feat: remove useless view bar on repertory list

--- Paroles/application/documents/RepertoryMusicTableViewCell.swift
+++ Paroles/application/documents/RepertoryMusicTableViewCell.swift
@@ -13,19 +13,11 @@ class RepertoryMusicTableViewCell: UITableViewCell {
     @IBOutlet weak var orderLabel: UILabel!
     @IBOutlet weak var musicNameLabel: UILabel!
     
-    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
-    
     var repertoryMusic: RepertoryMusic? {
         didSet {
             orderLabel.text = "\((repertoryMusic?.index ?? 0) + 1)"
             musicNameLabel.text = repertoryMusic?.music?.name
         }
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        widthConstraint.constant = 0.5
-        layoutIfNeeded()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
